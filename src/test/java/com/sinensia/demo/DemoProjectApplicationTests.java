1487497abb8570778feb70d36c06c6fd10b9d73c
Xavi Garcia <xavigarcia18@gmail.com>
2022-01-27 12:47:22 +0100

    Added rootTest

--- src/test/java/com/sinensia/demo/DemoProjectApplicationTests.java
+++ src/test/java/com/sinensia/demo/DemoProjectApplicationTests.java
@@ -1,13 +1,30 @@
 package com.sinensia.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebClient
 class DemoProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void rootTest(@Autowired TestRestTemplate restTemplate){
+		/*String ret = restTemplate.getForObject("/", String.class);
+		boolean b = ret == "Esta es la página inicial de Xavi Garcia!";
+		if(!b){
+			throw new Exception("Algo")
+		}*/
+
+		assertThat(restTemplate.getForObject("/", String.class)).isEqualTo("Esta es la página inicial de Xavi Garcia!");
+	}
 }
